a30cda3c768ed314901541cf1d7519010c02b232
Zohaib Butt <zohaibb@stud.ntnu.no>
2018-04-04 16:05:34 +0200

    Task2 done

--- Task2/procmi.ps1
+++ Task2/procmi.ps1
@@ -6,15 +6,16 @@ if($args.Count -gt 0) {
 
     for($i=0; $i -lt $args.Count; $i++) {
 
-        $fileName = $args[$i]+"--"+$date+".txt"
+        $fileName = ""+$args[$i]+"--"+$date+".txt"
         $location = (Get-Location).Path
+        $mem = (Get-Process -id $args[$i]).VirtualMemorySize / 1MB
+        $ws = (Get-Process -id $args[$i]).WorkingSet 
         New-Item -Path . -Itemtype File -Name $fileName  -Force
         Add-Content -Path $fileName -Value (" 
-        *********** Minne info om prosess med PID " + $args[$i] + " ***********
-        Total bruk av virtuelt minne:
-        Størrelse på Working Set:"
+        *********** Minne info om prosess med PID " + $args[$i] + " ***********`n" +
+        "Total bruk av virtuelt minne: " + $mem + "MB`n" +
+        "Størrelse på Working Set:" + $ws + "KB"
         )
-    
     }
          
 } else {
